da5f474830ba1f1c09817fdef7aebe64972027b8
PriyankaBhattad15 <priyankabhattad15@gmail.com>
2021-05-12 20:38:35 +0530

    files added

---
+++ Gitproject/src/com/abc/MainCla.java
@@ -0,0 +1,9 @@
+package com.abc;
+
+public class MainCla {
+	public static void main(String[] args) {
+		System.out.println("hi i am priya");
+		System.out.println("how are you");
+	}
+
+}
---
+++ Gitproject/src/com/mindtree/Demo.java
@@ -0,0 +1,9 @@
+package com.mindtree;
+
+public class Demo {
+	public void Method() {
+		System.out.println("who are you");
+		System.out.println("how you doin");
+	}
+
+}
